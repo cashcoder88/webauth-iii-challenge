1ec5faa80cf2209db07f7cd3f675cc1336b353f3
cashcoder88 <cashglobe@gmail.com>
2019-06-12 14:54:27 -0600

    POST /login

--- authentication/authRouter.js
+++ authentication/authRouter.js
@@ -21,6 +21,28 @@ router.post('/register', (req, res) => {
     })
 });
 
+router.post('/login', (req, res) => {
+    let {username, password} = req.body;
+
+    Users.findBy({username})
+    .first()
+    .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+            const token = makeToken(user)
+
+            res.status(200).json({
+                message: `Welcome ${user.username}!`,
+                token
+            });
+        } else {
+            res.status(401).json({ message: 'Invalid or nonexistent token'});
+        }
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+})
+
 function makeToken(user) {
     const payload = {
         subject: user.id,
